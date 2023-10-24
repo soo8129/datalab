b5eea6bfd7c4a85671c6f17491cdb6bb8f483e67
Kim Soo Hyeok <soohyeok_kim@tmax.co.kr>
2023-10-24 17:54:30 +0900

    Feat: 분류 cid 가져오는 유틸 추가

---
+++ src/datalab_scraper/utils/enum.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+class NaverCategory:
+    def __init__(self):
+        self.path = os.getcwd()
+
+        with open(self.path + "/src/datalab_scraper/json/cid_list.json", "r", encoding="UTF-8") as f:
+            self.cate_dict = json.load(f)['category_list']
+
+    def get_cid_list(self):
+        cid_list = []
+        for cate_lev1 in self.cate_dict['childList']:
+            cid_list.append((cate_lev1['cid'], cate_lev1['name']))
+            for cate_lev2 in cate_lev1['childList']:
+                cid_list.append((cate_lev2['cid'], cate_lev2['name']))
+                for cate_lev3 in cate_lev2['childList']:
+                    cid_list.append((cate_lev3['cid'], cate_lev3['name']))
+        return cid_list
+        
+def main():
+    tmp_list = NaverCategory().get_cid_list()
+    print(tmp_list)
+    
+if __name__ == '__main__':
+    main()
