a6c88b0e5e60b00c12f2276e461aa46d88f6eee3
Kim Soo Hyeok <soohyeok_kim@tmax.co.kr>
2023-10-25 14:54:46 +0900

    Feat: 기본 로거 객체 생성 datalab_scraper

--- src/datalab_scraper/datalab_scraper.py
+++ src/datalab_scraper/datalab_scraper.py
@@ -1,2 +1,16 @@
+from utils.NaverCategory import NaverCategory
+
 def _datalab_scraper():
-    return
+    while True:
+        try:
+            urls = NaverCategory().get_cid_list()
+            # rank_data = 
+            # db_insert(rank_data)
+        except Exception as e:
+            pass
+
+def main():
+    _datalab_scraper()
+
+if __name__ == '__main__':
+    main()
