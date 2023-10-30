78c1c922104aaf74c9323561aba26453438aa599
eunsong_lee <eunsong_lee@tmax.co.kr>
2023-10-30 10:19:09 +0900

    Chore: 로그 디렉토리 생성

--- src/datalab_scraper/utils/logger_config.py
+++ src/datalab_scraper/utils/logger_config.py
@@ -1,6 +1,11 @@
 # 로거 객체 하나로 통일시 사용
 import logging
 import logging.handlers
+import os
+
+log_directory = 'src/datalab_scraper/logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
 
 filehandler = logging.handlers.TimedRotatingFileHandler(filename='src/datalab_scraper/logs/datalab_logfile_', when='midnight', interval=1,
                                                     encoding='utf-8')
