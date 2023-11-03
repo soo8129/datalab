b853efadde9098fd7ab6161857b0426e50d5d250
Kim Soo Hyeok <soohyeok_kim@tmax.co.kr>
2023-11-03 15:47:28 +0900

    Build: setup.py requirements.txt 파일 정의

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='example',
+    name='datalab',
     version='0.1.0',
-    packages=find_packages(include=['exampleproject', 'exampleproject.*'])
+    author='soohyeok_kim',
+    author_email='soohyeok_kim@tmax.co.kr',
+    url='http://192.168.2.250:11088/fc/datalab_scraper',
+    description='네이버 데이터랩 인기검색어 스크래퍼',
+    packages=find_packages(where='src', include=['datalab_scraper', 'datalab_scraper.*']),
+    install_requires=requirements,
 )
